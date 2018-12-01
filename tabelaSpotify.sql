57414b2c08412e67a75b409432ea84bc140a8483
birromer <hummescmpa@gmail.com>
2018-12-01 15:49:35 -0200

    updated familiar plans and connection between users and playlists

--- tabelaSpotify.sql
+++ tabelaSpotify.sql
@@ -3,9 +3,9 @@ create table PLANO
 (
     codplano char(15) not null primary key,
     usuario char(15) not null unique,
+    tipoPlano char(1) not null,
     endereco varchar(50),
     cpf integer(11) unique,
-    tipoPlano char(1) not null,
     vinculoIE date,
     foreign key (tipoPlano) references PRECO_PLANO
         on update cascade,
@@ -15,24 +15,49 @@ create table PLANO
 );
 
 create table PRECO_PLANO
-(   
+(
     tipoPlano char(1) not null primary key,
-    preco numeric(5) not null   
+    preco numeric(5) not null
 );
 
-create table ASSINATURA
+create table FATURA
+(
+    codfat char(15) not null,
+    codplano char(15),
+    dataVencimento date not null,
+    valorFatura numeric(5) not null,
+    foreign key (codplano) references PLANO
+        on delete set null
+        on update cascade,
+    primary key(codplano, dataVencimento);
+);
+
+create table PLANO_FAMILIAR
 (
     codplano char(15) not null,
-    codu char(15) not null,
-    valorFatura numeric(5) not null,
-    dataVencimento date not null
+    familiar1 char(15),
+    familiar2 char(15),
+    familiar3 char(15),
+    familiar4 char(15),
+    familiar5 char(15),
     foreign key (codplano) references PLANO
         on delete cascade
         on update cascade,
-    foreign key (codu) references USUARIO
-	on delete cascade
+    foreign key (familiar1) references USUARIO
+        on delete set null
         on update cascade,
-    primary key(codplano, codu);
+    foreign key (familiar2) references USUARIO
+        on delete set null
+        on update cascade,
+    foreign key (familiar3) references USUARIO
+        on delete set null
+        on update cascade,
+    foreign key (familiar4) references USUARIO
+        on delete set null
+        on update cascade,
+    foreign key (familiar5) references USUARIO
+        on delete set null
+        on update cascade,
 );
 
 create table USUARIO
@@ -70,7 +95,7 @@ create table ALBUM
     anoLancamento date not null,
     nome varchar(30) not null,
     codart char(15) not null,
-    foreign key (codart) references ARTISTA on delete cascade 
+    foreign key (codart) references ARTISTA on delete cascade
 );
 
 create table MUSICA
@@ -88,8 +113,8 @@ create table PLAYLIST
 (
     codp char(15) primary key,
     nome varchar(100) not null,
-    
-    descricao varchar(300)    
+
+    descricao varchar(300)
 );
 
 create table PLAYLIST_CONTEUDO
@@ -97,8 +122,21 @@ create table PLAYLIST_CONTEUDO
     codm char(15) not null,
     codp char(15) not null,
     primary key (codm, codp),
-    foreign key(codm) references MUSICA,
+    foreign key(codm) references MUSICA
+        on delete cascade,
     foreign key(codp) references PLAYLIST
+        on delete cascade
+);
+
+create table PLAYLISTS_PUBLICAS_U
+(
+    codu varchar(15) not null,
+    codp char(15) not null,
+    primary key (codm, codp),
+    foreign key(codu) references USUARIO
+        on delete cascade,
+    foreign key(codp) references PLAYLIST
+        on delete cascade
 );
 
 create table BIBLIOTECA
